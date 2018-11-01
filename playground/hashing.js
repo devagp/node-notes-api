8a7c5cdd09a20845a810c026f86c011f6d54f4cc
Jadav Bheda <jadav@localcoinswap.com>
2018-11-01 17:33:20 +1100

    Hash user pasassword before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,33 @@
 const { SHA256 } = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var data = {
-  id: 10
-};
+var password = "123abc!";
 
-var token = jwt.sign(data, "123abc");
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-console.log(token);
+var hashedPassword =
+  "$2a$10$pcBwPemmq.M0xlPm/ml8G.wsSMUiTxEMrFveJnNy6EBBFWD4Yq7Sq";
 
-var decoded = jwt.verify(token, "123ab");
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
-console.log(decoded);
+// var data = {
+//   id: 10
+// };
+
+// var token = jwt.sign(data, "123abc");
+
+// console.log(token);
+
+// var decoded = jwt.verify(token, "123ab");
+
+// console.log(decoded);
 
 // var message = "I am user number 3";
 
