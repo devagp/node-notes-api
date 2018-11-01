2025cb22189488b2f4ef877f6d9b0e298862baed
Jadav Bheda <jadav@localcoinswap.com>
2018-11-01 17:15:13 +1100

    GET users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
+var { authenticate } = require("./middleware/authenticate");
 
 var app = express();
 
@@ -98,6 +99,10 @@ app.patch("/todos/:id", (req, res) => {
 
     res.send({ todo });
   });
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 //POST User /user
